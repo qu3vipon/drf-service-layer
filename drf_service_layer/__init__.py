d050619ee10b88d48190366acfad5b9239c6bc87
qu3vipon <sdh5813@gmail.com>
2021-09-19 15:04:19 +0900

    config: update config

--- drf_service_layer/__init__.py
+++ drf_service_layer/__init__.py
@@ -1,3 +1,3 @@
-"""Simple package supports service-layered design for Django REST Framework."""
+"""Simple package which supports service-layered design for Django REST Framework."""
 
 __version__ = '0.0.2'
